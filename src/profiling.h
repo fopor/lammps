2eff043f55117bcd128ab75b21b89ef52ad8f320
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2020-06-08 20:22:21 -0300

    Add HEADER file to hold paramount iteration profiling variable declarations

---
+++ src/profiling.h
@@ -0,0 +1,36 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   Profiling and instrumentation routines;
+   Not part of LAMMPS application!
+
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef MO833_PROFILING
+#define MO833_PROFILING
+
+#include<sys/time.h>
+
+double getCurrSecond();
+extern int maxPI;
+extern int PICount;
+extern double initTimeStamp;
+extern double elapsedParIterTime;
+extern double parEndTimeStamp;
+extern double parInitTimeStamp;
+
+#endif
+
+/* ERROR/WARNING messages:
+
+This module cannot detect or recover from any error;
+
+*/
