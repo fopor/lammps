02f926f39b7f45703b622e19d2d9ada3e5ab6238
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2020-06-14 12:52:44 -0300

    Fix style identation

--- src/main.cpp
+++ src/main.cpp
@@ -35,10 +35,10 @@ using namespace LAMMPS_NS;
 ------------------------------------------------------------------------- */
 
 double getCurrSecond() {
-    struct timeval tp;
-    struct timezone tzp;
-    gettimeofday(&tp,&tzp);
-    return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+  struct timeval tp;
+  struct timezone tzp;
+  gettimeofday(&tp,&tzp);
+  return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
 }
 
 void printProfInfo(){
